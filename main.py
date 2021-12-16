4b3f929c200b149f231027ea4e12810794e86888
Filomena <lyubaredbox@gmail.com>
2021-12-16 14:50:36 +0300

    Сундучок 1

--- main.py
+++ main.py
@@ -4,46 +4,46 @@ import random
 
 from pygame.sprite import Sprite
 pygame.init()
-main_dict = {1:(650, 650, 100, 100),
-40:(594, 650, 56, 100),
-39:(539, 650, 56, 100),
-38:(482, 650, 56, 100),
-37:(426, 650, 56, 100),
-36:(370, 650, 56, 100),
-35:(314, 650, 56, 100),
-34:(258, 650, 56, 100),
-33:(202, 650, 56, 100),
-32:(150, 650, 52, 100),
-31:(50, 650, 100, 100),
-30:(50, 594, 100, 56),
-29:(50, 539, 100, 56),
-28:(50, 482, 100, 56),
-27:(50, 426, 100, 56),
-26:(50, 370, 100, 56),
-25:(50, 314, 100, 56),
-24:(50, 258, 100, 56),
-23:(50, 202, 100, 56),
-22:(50, 150, 100, 52),
-21:(50, 50, 100, 100),
-20:(150, 50, 56, 100),
-19:(206, 50, 56, 100),
-18:(262, 50, 56, 100),
-17:(318, 50, 56, 100),
-16:(374, 50, 56, 100),
-15:(430, 50, 56, 100),
-14:(486, 50, 56, 100),
-13:(542, 50, 56, 100),
-12:(596, 50, 54, 100),
-11:(650, 50, 100, 100),
-10:(650, 150, 100, 56),
-9:(650, 206, 100, 56),
-8:(650, 262, 100, 56),
-7:(650, 318, 100, 56),
-6:(650, 374, 100, 56),
-5:(650, 430, 100, 56),
-4:(650, 486, 100, 56),
-3:(650, 542, 100, 56),
-2:(650, 596, 100, 54),
+main_dict = {1:(650, 650, 100, 100, "GO"),
+40:(594, 650, 56, 100, "BOARDWALK"),
+39:(539, 650, 56, 100, "LUXURY TAX"),
+38:(482, 650, 56, 100, "PARK PLACE"),
+37:(426, 650, 56, 100, "CHANCE"),
+36:(370, 650, 56, 100, "SHORT LINE"),
+35:(314, 650, 56, 100, "PENNSYLVANIA AVENUE"),
+34:(258, 650, 56, 100, "COMMUNITY CHEST"),
+33:(202, 650, 56, 100, "NORTH CAROLINA AVENUE"),
+32:(150, 650, 52, 100, "PACIFIC AVENUE"),
+31:(50, 650, 100, 100, "GO TO JAIL"),
+30:(50, 594, 100, 56, "MARVIN GARDENS"),
+29:(50, 539, 100, 56, "WATER WORKS"),
+28:(50, 482, 100, 56, "VENTNOR AVENUE"),
+27:(50, 426, 100, 56, "ATLANTIC AVENUE"),
+26:(50, 370, 100, 56, "B. & O. RAILROAD"),
+25:(50, 314, 100, 56, "ILLINOIS AVENUE"),
+24:(50, 258, 100, 56, "INDIANA AVENUE"),
+23:(50, 202, 100, 56, "CHANCE"),
+22:(50, 150, 100, 52, "KENTUCKY AVENUE"),
+21:(50, 50, 100, 100, "FREE PARKING"),
+20:(150, 50, 56, 100, "NEW YORK AVENUE"),
+19:(206, 50, 56, 100, "TENNESSEE AVENUE"),
+18:(262, 50, 56, 100, "COMMUNITY CHEST"),
+17:(318, 50, 56, 100, "ST. JAMES PLACE"),
+16:(374, 50, 56, 100, "PENNSYLVANIA RAILROAD"),
+15:(430, 50, 56, 100, "VIRGINIA AVENUE"),
+14:(486, 50, 56, 100, "STATES AVENUE"),
+13:(542, 50, 56, 100, "ELECTRIC COMPANY"),
+12:(596, 50, 54, 100, "ST. CHARLES PLACE"),
+11:(650, 50, 100, 100, "JUST VISITING"),
+10:(650, 150, 100, 56, "CONNECTICUT AVENUE"),
+9:(650, 206, 100, 56, "VERMONT AVENUE"),
+8:(650, 262, 100, 56, "CHANCE"),
+7:(650, 318, 100, 56, "ORIENTAL AVENUE"),
+6:(650, 374, 100, 56, "READING RAILROAD"),
+5:(650, 430, 100, 56, "INCOME TAX"),
+4:(650, 486, 100, 56, "BALTIC AVENUE"),
+3:(650, 542, 100, 56, "COMMUNITY CHEST"),
+2:(650, 596, 100, 54, "MEDITERRANEAN AVENUE"),
 'main':(320, 320, 180, 180)
 }
 
@@ -102,6 +102,18 @@ class Chip(Sprite):
             text1 = f1.render(f'{sum(num)} ходов', True, (0, 255, 0))
         screen.blit(text2, (800, 100))
         screen.blit(text1, (800, 150))
+        if main_dict[self.cordinate_chip][4] == "COMMUNITY CHEST":
+            self.community_chest()
+
+    def community_chest(self):
+        chest = open("data/chest.txt")
+        chest_list = chest.read().split("\n")
+        f3 = pygame.font.Font(None, 50)
+        text3 = f3.render("Общественная казна:", True, (255, 0, 0))
+        screen.blit(text3, (800, 200))
+        f4 = pygame.font.Font(None, 50)
+        text4 = f4.render(random.choice(chest_list), True, (0, 255, 0))
+        screen.blit(text4, (800, 250))
 
 
 chip_red = Chip((0, 0), 'fishka.png', 1)
